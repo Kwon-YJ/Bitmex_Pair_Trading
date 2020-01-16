71a8d0d398e7f4261766cd2ed78b8d7e02a691b0
your Name <kyj1435@gmail.com>
2020-01-16 05:17:56 +0000

    Test commit

--- Main.py
+++ Main.py
@@ -10,22 +10,23 @@ basis = []
 
 
 def calculate(ticker1, ticker2, time_):
-	ticker1_ohlcvs = bitmex.fetch_ohlcv(ticker1, "1m",bitmex.parse8601(time_))
-	ticker2_ohlcvs = bitmex.fetch_ohlcv(ticker2, "1m", bitmex.parse8601(time_))
-	for i in range(0,len(ticker1_ohlcvs)):
-		basis.append(str(datetime.datetime.fromtimestamp(ticker1_ohlcvs[i][0]/1000)))
-		basis.append(ticker2_ohlcvs[i][1]/ticker1_ohlcvs[i][1])
-		basis.append(ticker2_ohlcvs[i][2]/ticker1_ohlcvs[i][2])
-		basis.append(ticker2_ohlcvs[i][3]/ticker1_ohlcvs[i][3])
-		basis.append(ticker2_ohlcvs[i][4]/ticker1_ohlcvs[i][4])
-	timestamp = ticker2_ohlcvs[-1][0]
-	datetimeobj = str(datetime.datetime.fromtimestamp(timestamp/1000))
-	nexttime = datetimeobj[0:10] + 'T' + datetimeobj[11:19]
-	try:
-		time.sleep(4.9)
-		calculate(ticker1, ticker2, nexttime)
-	except:
-		print(" ")
+    time.sleep(5.01)
+    ticker1_ohlcvs = bitmex.fetch_ohlcv(ticker1, "1h",bitmex.parse8601(time_))
+    ticker2_ohlcvs = bitmex.fetch_ohlcv(ticker2, "1h", bitmex.parse8601(time_))
+    for i in range(0,len(ticker1_ohlcvs)):
+        basis.append(str(datetime.datetime.fromtimestamp(ticker1_ohlcvs[i][0]/1000)))
+        basis.append(ticker2_ohlcvs[i][1]/ticker1_ohlcvs[i][1])
+        basis.append(ticker2_ohlcvs[i][2]/ticker1_ohlcvs[i][2])
+        basis.append(ticker2_ohlcvs[i][3]/ticker1_ohlcvs[i][3])
+        basis.append(ticker2_ohlcvs[i][4]/ticker1_ohlcvs[i][4])
+        timestamp = ticker2_ohlcvs[-1][0]
+        datetimeobj = str(datetime.datetime.fromtimestamp(timestamp/1000))
+        nexttime = datetimeobj[0:10] + 'T' + datetimeobj[11:19]
+    try:
+        time.sleep(5.01)
+        calculate(ticker1, ticker2, nexttime)
+    except:
+        print(" ")
 
 start = '2019-12-13T17:45:00'
 #start = '2020-01-10T00:00:00'
@@ -37,7 +38,6 @@ now_ = str(datetime.datetime.now() + datetime.timedelta(hours = 9))
 remaining =str(int((float(now) - float(convert_date))/72000))
 print("현재시간" +"["+now_[11:16]+"]")
 print("소요시간 최소" + remaining + "분")
-
 
 
 calculate('BTC/USD', 'XBTM20', start)
